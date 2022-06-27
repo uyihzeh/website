aac6167254b16215de4948daa15bb01ae4ce5370
uyihzeh <81477789+uyihzeh@users.noreply.github.com>
2022-06-27 18:50:39 +0800

    Add files via upload

---
+++ postReg.php
@@ -0,0 +1,8 @@
+<?php
+	$username = $_POST['username'];  # 用户名
+	$pw = $_POST['pw'];  # 密码
+	$cpw = $_POST['cpw'];  # 确认密码
+	$sex = $_POST['sex']  # 性别
+	$fav = $_POST['fav'];  # 爱好 
+	# echo "您输入的用户名是:" . $username . <br>;
+?>
